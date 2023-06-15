940bf71cc57901c40e4309a057a636c81902c4d4
Kapil Rajesh Kavitha <kapil.rajesh@students.iiit.ac.in>
2023-06-15 13:48:48 +0300

    Added files for tokenize-pos-align code
    - Input file (unique__utterances_en_hi_transltions.json) and output file(train_unique_utterances_en_hi_transltions_token_pos_alignments.json) added
    - Updated python code added
    - Example notebook with the implementation is also provided.

--- tokenize-pos-awesoome-align.py
+++ tokenize-pos-awesoome-align.py
@@ -1,59 +1,32 @@
 import os
-import sys
-import json
 import pandas as pd
-import pickle 
-import numpy as np
-from collections import Counter
-
 
 from tqdm import tqdm
 tqdm.pandas()
 
-
-from nltk.tokenize import sent_tokenize
-from nltk.tree import Tree
-from nltk.draw.tree import TreeView
-from nltk.tokenize import sent_tokenize
-from conllu import parse
-
-from indicnlp.tokenize import sentence_tokenize
-from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
-
-
 import stanza
 
-import re
-import requests
-import argparse
+# downloading stanza models for indian languages & english
+for i in ["en", "hi", "mr", "ta", "te"]:
+    stanza.download(i)
 
-import traceback
+cwd = os.getcwd()
 
-os.getcwd()
-
+# TOKENIZE-POS CODE
 
 df_translations_set = pd.read_json("unique_utterances_en_hi_transltions.json")
-# df_translations_set = df_translations_set[:1]
-print(df_translations_set.shape)
-# df_translations_set.head()
 
-
-nlp_en = stanza.Pipeline(lang='en', processors='tokenize, pos')
-nlp_hi = stanza.Pipeline(lang = 'hi', processors='tokenize, pos')
-
-
-sys.path.append("/scratch/anmol.goel/stanford-tod/codemix-gcm/")
-
+# setting up awesome-align aligner
 from auxilaries.utils import awesomealign
 
+aligner = awesomealign(modelpath = 'bert-base-multilingual-cased',
+                      tokenizerpath = 'bert-base-multilingual-cased')  
 
-aligner = awesomealign(modelpath = '/scratch/anmol.goel/stanford-tod/codemix-gcm/awesome-align-sbatch/awesome-align-fine-tuned-savedmodel',
-                      tokenizerpath = '/scratch/anmol.goel/stanford-tod/codemix-gcm/awesome-align-sbatch/awesome-align-fine-tuned-savedmodel')  
-
-
-def get_stanza_info(text, model):
-    
-    doc = model(text)
+# setting up stanza pipelines
+def get_stanza_info(text, language): #TO ADD - language parameter
+    # language accepts - en, hi, ta, te
+    nlp_lang = stanza.Pipeline(lang=language, processors='tokenize, pos')
+    doc = nlp_lang(text)
     
     sents, tokens, postags = [], [], []
     
@@ -66,27 +39,7 @@ def get_stanza_info(text, model):
            "tokens" : tokens,
            "postags" : postags}
 
-
-en_tokenized, en_pos, hi_tokenized, hi_pos = [], [], [], []
-
-for ind, row in tqdm(df_translations_set.iterrows()):
-    
-    eng_feats = get_stanza_info(row["en"], nlp_en)
-    hin_feats = get_stanza_info(row["hi"], nlp_hi)
-    
-    en_tokenized.append(eng_feats["tokens"])
-    hi_tokenized.append(hin_feats["tokens"])    
-    
-    en_pos.append(eng_feats["postags"])
-    hi_pos.append(hin_feats["postags"])    
-
-#print(f"en tokens : {eng_feats['tokens']}")
-
-df_translations_set["en_tokens"] = en_tokenized
-df_translations_set["en_pos"] = en_pos
-df_translations_set["hi_tokens"] = hi_tokenized
-df_translations_set["hi_pos"] = hi_pos
-
+# creating token alignment map
 def create_alignments_token_map(sent_src, sent_tgt, alignments):
     
     token_map = {}
@@ -106,24 +59,48 @@ def create_alignments_token_map(sent_src, sent_tgt, alignments):
     
     return token_map
 
-
-
-def get_alingment_token_map(en_sent, hi_sent):
+# getting alignments and token map
+def get_alignment_token_map(en_sent, hi_sent):
     alignments = aligner.get_alignments_sentence_pair(en_sent, hi_sent)
     token_alignment_map = create_alignments_token_map(en_sent, hi_sent, alignments)
     return alignments, token_alignment_map
 
+# main code
+lang1_tokenized, lang1_pos, lang2_tokenized, lang2_pos = [], [], [], []
+
+for ind, row in tqdm(df_translations_set.iterrows()):
+    
+    languages = row.keys()   
+    lang1, lang2 = [str(lang) for lang in languages]
+
+    lang1_feats = get_stanza_info(row[lang1], lang1)
+    lang2_feats = get_stanza_info(row[lang2], lang2)
+    
+    lang1_tokenized.append(lang1_feats["tokens"])
+    lang2_tokenized.append(lang2_feats["tokens"])    
+    
+    lang1_pos.append(lang1_feats["postags"])
+    lang2_pos.append(lang2_feats["postags"])    
+
+#print(f"en tokens : {lang1_feats['tokens']}")
+df_translations_set["lang1"] = lang1
+df_translations_set["lang1_tokens"] = lang1_tokenized
+df_translations_set["lang1_pos"] = lang1_pos
+
+df_translations_set["lang2"] = lang2
+df_translations_set["lang2_tokens"] = lang2_tokenized
+df_translations_set["lang2_pos"] = lang2_pos
+
 list_of_alignments, list_of_token_alignment_map = [], []
 
 for ind, row in tqdm(df_translations_set.iterrows()):
-    #print(row["en_tokens"])
-    en_tokenized_sent = [" ".join(sent_list) for sent_list in row["en_tokens"]]
-    hi_tokenized_sent = [" ".join(sent_list) for sent_list in row["hi_tokens"]]
+    lang1_tokenized_sent = [" ".join(sent_list) for sent_list in row["lang1_tokens"]]
+    lang2_tokenized_sent = [" ".join(sent_list) for sent_list in row["lang2_tokens"]]
     alignment_row, token_alignment_map_row = [], []
     try:
-        assert len(en_tokenized_sent) == len(hi_tokenized_sent)
-        for ensent, hisent in zip(en_tokenized_sent, hi_tokenized_sent):
-            alignments, token_alignment_map = get_alingment_token_map(ensent, hisent)
+        assert len(lang1_tokenized_sent) == len(lang2_tokenized_sent)
+        for lang1sent, lang2sent in zip(lang1_tokenized_sent, lang2_tokenized_sent):
+            alignments, token_alignment_map = get_alignment_token_map(lang1sent, lang2sent)
             alignment_row.append(alignments)
             token_alignment_map_row.append(token_alignment_map)
         list_of_alignments.append(alignment_row)
@@ -134,10 +111,46 @@ for ind, row in tqdm(df_translations_set.iterrows()):
         list_of_alignments.append(None)
         list_of_token_alignment_map.append(None)
 
-
-
 df_translations_set["alignments_awesomealign"] = list_of_alignments
 df_translations_set["token_alignment_map_awesomealign"] = list_of_token_alignment_map
+
+#generate alignments for POS tags
+
+#heuristic
+def replace_noun_adj_single_aligned(sent, postags, token_map):
+    
+    codemixcandidate = ""
+    
+    for token, token_pos in zip(sent, postags):
+        if "NOUN" in token_pos or "ADJ" in token_pos or "PROPN" in token_pos:
+            if token in token_map:
+                codemixcandidate += f" {token_map[token]}"
+            else:
+                codemixcandidate += f" {token}"                
+        else:
+            codemixcandidate += f" {token}"
+            
+    return codemixcandidate
+
+def get_codemix_candidate(row):
+    sentence = ""
+    for en_sent, en_pos, hi_sent, hi_pos, alignments, token_alignment_map in zip(row["lang1_tokens"], row["lang1_pos"], row["lang2_tokens"], row["lang2_pos"], row["alignments_awesomealign"], row["token_alignment_map_awesomealign"]):
+        sentence += replace_noun_adj_single_aligned(hi_sent, hi_pos, token_alignment_map)
+    return sentence
+
+            
+def get_codemix_candidates_for_dataframe(df):
+    codemix_candidates = []
+
+    for ind, row in tqdm(df.iterrows()):
+        cm = get_codemix_candidate(row)
+        codemix_candidates.append(cm)
+        
+    return codemix_candidates
+
+codemix_candidates = get_codemix_candidates_for_dataframe(df_translations_set)
+
+df_translations_set["codemixed-sentences"] = codemix_candidates
 
 
 df_translations_set.to_json("train_unique_utterances_en_hi_transltions_token_pos_alignments.json", 
@@ -145,3 +158,5 @@ df_translations_set.to_json("train_unique_utterances_en_hi_transltions_token_pos
                                          orient = "records",
                                         indent = 4)
 
+
+print("done")
